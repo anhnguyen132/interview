455998df4fdc64cb57c093468d7452c6bdea16f0
Anh Nguyen <anh.h.nguyen132@gmail.com>
2024-04-22 15:13:46 -0700

    cycle sort 1 to N distinct values using a single while loop

--- cycle_sort_1toN.py
+++ cycle_sort_1toN.py
@@ -6,44 +6,45 @@ from typing import List
 def cycleSort1toN(nums: List[int]) -> None:
     """
     cycle sort for special case where all elements are of value between 1 and n (all distinct values)
+    => For an elem x, its index in the sorted array = x - 1
+    i.e. index for 1 is 0, index for 2 is 1, etc.
     """
     n = len(nums)
-    # writes = 0
+    i = 0
+    while i < n:
+        cur_elem = nums[i]
+        correctIndex = nums[i] - 1
+        if correctIndex != i:
+            nums[i] = nums[correctIndex]
+            nums[correctIndex] = cur_elem
+        else:
+            i += 1
 
-    def findPos(cycle_start: int, elem: int) -> int:
-        pos = elem - 1
+    # n = len(nums)
+    # # writes = 0
 
-        if pos == cycle_start:
-            return pos
+    # for cycle_start in range(n - 1):
+    #     # find the intended position for nums[cycle_start] in sorted nums
+    #     pos = nums[cycle_start] - 1
 
-        while nums[pos] == elem:
-            pos += 1
+    #     # if item is already at its intended position, skip the cycle
+    #     if pos == cycle_start:
+    #         continue
 
-        return pos
+    #     # update the current elem to find its intended position in sorted nums
+    #     cur_elem = nums[pos]
+    #     nums[pos] = nums[cycle_start]
+    #     # writes += 1
 
-    for cycle_start in range(n - 1):
-        # find the intended position for nums[cycle_start] in sorted nums
-        pos = findPos(cycle_start, nums[cycle_start])
+    #     # repeat the cycle until we find the elem in nums where its intended position is cycle_start
+    #     while pos != cycle_start:
+    #         pos = cur_elem - 1
 
-        # if item is already at its intended position, skip the cycle
-        if pos == cycle_start:
-            continue
+    #         # put cur_elem to its intended position & update the current elem
+    #         nums[pos], cur_elem = cur_elem, nums[pos]
+    #         # writes += 1
 
-        # update the current elem to find its intended position in sorted nums
-        cur_elem = nums[pos]
-        nums[pos] = nums[cycle_start]
-        # writes += 1
-
-        # repeat the cycle until we find the elem in nums where its intended position is cycle_start
-        while pos != cycle_start:
-            pos = findPos(cycle_start, cur_elem)
-            # print(nums, cycle_start, pos)
-
-            # put cur_elem to its intended position & update the current elem
-            nums[pos], cur_elem = cur_elem, nums[pos]
-            # writes += 1
-
-        # return writes
+    #     # return writes
 
 
 a = [3, 2, 4, 5, 1]
