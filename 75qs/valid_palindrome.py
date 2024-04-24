254e04b7e9554210e89244ad44ef46aba9fe5f74
Anh Nguyen <anh.h.nguyen132@gmail.com>
2024-04-23 21:07:44 -0700

    merging valid palindrom files

--- 75qs/valid_palindrome.py
+++ 75qs/valid_palindrome.py
@@ -2,20 +2,40 @@
 
 
 def isPalindrome(s: str) -> bool:
-    # time O(n), space O(1)
+    """
+    2 pointers meet in the middle
+
+    Time O(n), Space O(1)
+    """
     l, r = 0, len(s) - 1
     while l < r:
-        if not s[l].isalnum():
-            l += 1
-        if not s[r].isalnum():
-            r -= 1
-        if s[l].isalnum() and s[r].isalnum():
-            if s[l].lower() != s[r].lower():
+        lc = s[l].lower()
+        rc = s[r].lower()
+        if lc.isalnum() and rc.isalnum():
+            if lc != rc:
                 return False
             l += 1
             r -= 1
+        else:
+            l += not lc.isalnum()
+            r -= not rc.isalnum()
 
     return True
+
+    # # time O(n), space O(1)
+    # l, r = 0, len(s) - 1
+    # while l < r:
+    #     if not s[l].isalnum():
+    #         l += 1
+    #     if not s[r].isalnum():
+    #         r -= 1
+    #     if s[l].isalnum() and s[r].isalnum():
+    #         if s[l].lower() != s[r].lower():
+    #             return False
+    #         l += 1
+    #         r -= 1
+
+    # return True
 
 
 # Explanation: "amanaplanacanalpanama" is a palindrome.
@@ -24,3 +44,5 @@ print(isPalindrome("race a car") == False)
 print(isPalindrome(" ") == True)
 print(isPalindrome("race a car 90asakshugf") == False)
 print(isPalindrome("i am a girl am i a girl") == False)
+print(isPalindrome("a") == True)
+print(isPalindrome("9 a ??b$$. a 9") == True)
