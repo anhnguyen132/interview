e03e21714b1a483d30d2a51ef9ec5a2c0016a3c7
Anh Nguyen <anh.h.nguyen132@gmail.com>
2024-05-03 11:16:16 -0700

    quick sort w/o swapping pivot to last position

--- quick_sort.py
+++ quick_sort.py
@@ -6,10 +6,11 @@ def swap(a: List, l: int, r: int) -> None:
 
 
 def partition(a: List, l: int, r: int, pivot: int) -> None:
+    # print(l, r, pivot, a)
     while l <= r:
-        while a[l] <= pivot and l <= r:
+        while l <= r and a[l] <= pivot:
             l += 1
-        while a[r] > pivot and l <= r:
+        while l <= r and a[r] > pivot:
             r -= 1
 
         if l < r:
@@ -17,7 +18,8 @@ def partition(a: List, l: int, r: int, pivot: int) -> None:
             # l += 1
             # r -= 1
 
-    return l
+    # return l
+    return r
 
 
 def quickSortHelper(a: List, l: int, r: int) -> None:
@@ -26,13 +28,14 @@ def quickSortHelper(a: List, l: int, r: int) -> None:
 
     pivot = a[l]
     # swap pivot w the last elem
-    swap(a, l, r)
+    # swap(a, l, r)
 
     # Partition the array to 2 parts where left part contains all numbers <= pivot
     # returns the index splitting these 2 partitions
-    split = partition(a, l, r - 1, pivot)
+    # split = partition(a, l, r - 1, pivot)
+    split = partition(a, l + 1, r, pivot)
     # swap back pivot
-    swap(a, split, r)
+    swap(a, split, l)
 
     # NOT (a, l, split)!!! Otherwise it'd fail if pivot = max number in a
     quickSortHelper(a, l, split - 1)
@@ -44,7 +47,7 @@ def quickSort(a: List) -> None:
     """
     Divide and Conquer
     Choose the first elem as pivot.
-    Swap the pivot with the last elem
+    Swap the pivot with the last elem (=> Can also not swap here, instead do the sorting in a[1:] then swap back pivot with elem at index = right pointer)
     Partition the array to 2 parts where left part contains all numbers <= pivot:
         Have left, right pointers = 0, n - 2 moving inwards, swap a[left] with a[right] if a[left] > pivot and a[right] < pivot
     Keep doing this recursively on the 2 new partitions
@@ -57,6 +60,10 @@ def quickSort(a: List) -> None:
     """
     quickSortHelper(a, 0, len(a) - 1)
 
+
+# a = [6, 1, 4, 9, 0, 3, 5, 2, 7, 8]
+# quickSort(a)
+# print(a == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 a = [1, 4, 0, 3, 5, 2]
 quickSort(a)
