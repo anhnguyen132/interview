1e73448d30ed7a496cc9bdebfd9b4b2429478b97
Anh Nguyen <anh.h.nguyen132@gmail.com>
2024-05-28 12:56:38 -0700

    add problem statements for tesla apr 2024

--- companies/tesla/april_2024/assembly_car.py
+++ companies/tesla/april_2024/assembly_car.py
@@ -1,3 +1,12 @@
+"""
+2) Given 2 arrays A, B of same length n where A[i] and B[i] are how much time it costs to assemble a car part on line A, B respectively. X = time it costs to move car assembling on line A to line B for its next part. Y = time it costs to move car assembling on line B to line A for its next part. Find the min time to assemble a car
+e.g. 
+assemble([1, 6, 2], [3, 2, 5], 2, 1) == 8
+bc 1 + (2+2) + (2+1) = 8    
+    A     B       A
+"""
+
+
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 def helper(A, B, X, Y, cur):
--- companies/tesla/april_2024/jump_frogs.py
+++ companies/tesla/april_2024/jump_frogs.py
@@ -1,3 +1,15 @@
+"""
+1) Given an array of post nums, find the max dist s.t. having 2 pointers l, r starting at a position, l can only go left and r and only go right and both can only move if next num >= current num. 
+E.g. 
+print(maxDist([2, 6, 8, 5]) == 3)
+print(maxDist([1, 5, 5, 2, 6]) == 4)print(maxDist([1, 1]) == 2)
+print(maxDist([1, 1, 1, 1, 1]) == 5)
+print(maxDist([2, 2, 2, 10, 1, 10, 2, 2, 2]) == 4)
+print(maxDist([2, 2, 2, 10, 1, 10, 2, 2]) == 4)
+print(maxDist([2, 10, 8, 4, 2, 1, 9, 10, 2]) == 7)
+"""
+
+
 def maxDist(blocks):
     # Time O(n), Space O(1)
     n = len(blocks)
